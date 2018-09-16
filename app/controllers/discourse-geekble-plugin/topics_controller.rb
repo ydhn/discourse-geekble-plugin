3bed0a7f022ad02d91184114a7bd1bedbde119ff
Yundo Han <han@yun.do>
2018-09-16 18:04:14 +0900

    fix wrong code in topics controller

--- app/controllers/discourse-geekble-plugin/topics_controller.rb
+++ app/controllers/discourse-geekble-plugin/topics_controller.rb
@@ -1,7 +1,7 @@
 module DiscourseGeekblePlugin
   class TopicsController < ApplicationController
     def show
-      topic = Topic.find(:id)
+      topic = Topic.find(params[:id])
       @posts = topic.posts.where("deleted_at IS NULL")
       render json: @posts
     end
