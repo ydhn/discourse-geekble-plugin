041a5662e6bdf280942a7216cf60900af9c02f26
Yundo Han <han@yun.do>
2018-09-16 17:14:00 +0900

    update card controller to contain remarkable post

--- app/controllers/discourse-geekble-plugin/cards_controller.rb
+++ app/controllers/discourse-geekble-plugin/cards_controller.rb
@@ -4,8 +4,16 @@ module DiscourseGeekblePlugin
 
     def index
       card_category = Category.find_by(slug: 'cards')
-      page = params[:page].to_i-1 rescue 0
-      @cards = card_category.topics.limit(PAGE_SIZE).offset(PAGE_SIZE * page).to_a
+      page = params[:page].to_i rescue 1
+      stmt = card_category.topics.where("deleted_at IS NULL")
+      stmt = stmt.limit(PAGE_SIZE).offset(PAGE_SIZE * (page-1))
+      @cards = []
+      stmt.each do |c|
+        card = c.as_json
+        remarkable_posts = c.posts.order('like_count desc').limit(1)
+        card[:remarkable_post] = remarkable_posts.first.as_json if remarkable_posts.size
+        @cards.push card
+      end
       render json: @cards
     end
   end
