66d40edc05f68a8a2ba6628cc349835a9c52f7e3
Yundo Han <han@yun.do>
2018-09-16 17:23:07 +0900

    update cards controller

--- app/controllers/discourse-geekble-plugin/cards_controller.rb
+++ app/controllers/discourse-geekble-plugin/cards_controller.rb
@@ -6,11 +6,12 @@ module DiscourseGeekblePlugin
       card_category = Category.find_by(slug: 'cards')
       page = params[:page].to_i rescue 1
       stmt = card_category.topics.where("deleted_at IS NULL")
+      stmt = stmt.order("last_posted_at desc")
       stmt = stmt.limit(PAGE_SIZE).offset(PAGE_SIZE * (page-1))
       @cards = []
       stmt.each do |c|
         card = c.as_json
-        remarkable_posts = c.posts.order('like_count desc').limit(1)
+        remarkable_posts = c.posts.where("like_count > 0").order('like_count desc').limit(1)
         card[:remarkable_post] = remarkable_posts.first.as_json if remarkable_posts.size
         @cards.push card
       end
