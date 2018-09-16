9050a90e27b38a607e039a0dfc48f60f346263ac
Yundo Han <han@yun.do>
2018-09-16 16:45:02 +0900

    modify pagination method

--- app/controllers/discourse-geekble-plugin/cards_controller.rb
+++ app/controllers/discourse-geekble-plugin/cards_controller.rb
@@ -3,7 +3,7 @@ module DiscourseGeekblePlugin
     def index
       card_category = Category.find_by(slug: 'cards')
       params[:page] = params[:page].to_i rescue 1
-      @cards = card_category.topics.page(params[:page]).per(20)
+      @cards = card_category.topics.paginate(per_page: 20, page: params[:page])
       render json: @cards
     end
   end
