3539c31f0c068f742d565c94231b6765cc612bdf
Yundo Han <han@yun.do>
2018-09-16 16:58:44 +0900

    fix bad route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ require_dependency "discourse_geekble_plugin_constraint"
 constraints = DiscourseGeekblePluginConstraint.new
 DiscourseGeekblePlugin::Engine.routes.draw do
   get "/hello" => "hello#index", constraints: constraints
-  get "/cards" => "cards#test", constraints: constraints, defaults: { format: :json }
+  get "/cards" => "cards#index", constraints: constraints, defaults: { format: :json }
 end
