8e6d1ffc890395a5de29df2cd6a58beb8057c7e8
Yundo Han <han@yun.do>
2018-09-16 16:20:30 +0900

    fix gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ require_dependency "discourse_geekble_plugin_constraint"
 
 constraints = DiscourseGeekblePluginConstraint.new
 DiscourseGeekblePlugin::Engine.routes.draw do
+  get "/hello" => "hello#index", constraints: constraints
   get "/cards" => "cards#test", constraints: constraints, defaults: { format: :json }
 end
