8eb74cbf88fae17b3ec2d3d1842d8d1cbf2abd9c
Yundo Han <han@yun.do>
2018-09-16 15:32:28 +0900

    add test api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require_dependency "discourse_geekble_plugin_constraint"
 
+constraints = DiscourseGeekblePluginConstraint.new
 DiscourseGeekblePlugin::Engine.routes.draw do
-  get "/hello" => "hello#index", constraints: DiscourseGeekblePluginConstraint.new
-  get "/test" => "hello#test", constraints: DiscourseGeekblePluginConstraint.new
+  get "/hello" => "hello#index", constraints: constraints
+  get "/test" => "hello#test", constraints: constraints, defaults: { format: :json }
 end
