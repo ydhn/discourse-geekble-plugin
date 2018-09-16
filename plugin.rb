23e06de082cfebc0346454fe15384a2ef1713473
Yundo Han <han@yun.do>
2018-09-16 19:09:27 +0900

    add test code to gem

--- plugin.rb
+++ plugin.rb
@@ -6,8 +6,8 @@
 # url: https://github.com/ydhn/discourse-geekble-plugin
 enabled_site_setting :discourse_geekble_plugin_enabled
 
-gem 'jbuilder', github: 'rails/jbuilder', branch: '2.0.x-stable'
-gem 'kaminari', github: 'kaminari/kaminari', branch: '1-0-stable'
+gem 'jbuilder', {github: 'rails/jbuilder', branch: '2.0.x-stable'}
+gem 'kaminari', {github: 'kaminari/kaminari', branch: '1-0-stable'}
 register_asset 'stylesheets/discourse-geekble-plugin.scss'
 
 load File.expand_path('../lib/discourse-geekble-plugin/engine.rb', __FILE__)
