4349c076fa5b0d2a60aed5a1b892c705e4c51e7f
Yundo Han <han@yun.do>
2018-09-16 18:23:19 +0900

    add user to topic

--- app/controllers/discourse-geekble-plugin/topics_controller.rb
+++ app/controllers/discourse-geekble-plugin/topics_controller.rb
@@ -2,11 +2,13 @@ module DiscourseGeekblePlugin
   class TopicsController < ApplicationController
     def show
       topic = Topic.find(params[:id])
-      posts = topic.posts.where("deleted_at IS NULL")
+      posts = topic.posts.where("deleted_at IS NULL").order("created_at asc").offset(1)
+      # 첫번째 original post를 제외함
       @posts = []
       posts.each do |p|
         post = p.as_json
         post[:like_count] = p.like_count
+        post[:user] = p.user.as_json
         @posts.push(post)
       end
       render json: @posts
