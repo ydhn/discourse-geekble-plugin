9c9681857229addaab939085f5beb800c891bac8
Yundo Han <han@yun.do>
2018-09-16 18:14:41 +0900

    add like count to post

--- app/controllers/discourse-geekble-plugin/topics_controller.rb
+++ app/controllers/discourse-geekble-plugin/topics_controller.rb
@@ -2,7 +2,13 @@ module DiscourseGeekblePlugin
   class TopicsController < ApplicationController
     def show
       topic = Topic.find(params[:id])
-      @posts = topic.posts.where("deleted_at IS NULL")
+      posts = topic.posts.where("deleted_at IS NULL")
+      @posts = []
+      posts.each do |p|
+        post = p.as_json
+        post[:like_count] = p.like_count
+        @posts.push(post)
+      end
       render json: @posts
     end
   end
