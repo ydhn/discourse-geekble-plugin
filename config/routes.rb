774163dc902dc7cfa04c0aff7a716a3e311b4bb1
Yundo Han <han@yun.do>
2018-10-02 16:53:41 +0900

    add top index for cards controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ require_dependency "discourse_geekble_plugin_constraint"
 constraints = DiscourseGeekblePluginConstraint.new
 DiscourseGeekblePlugin::Engine.routes.draw do
   get "/hello" => "hello#index", constraints: constraints
-  get "/cards" => "cards#index", constraints: constraints, defaults: { format: :json }
+  get "/cards" => "cards#recent_index", constraints: constraints, defaults: { format: :json }
+  get "/cards/top" => "cards#top_index", constraints: constraints, defaults: { format: :json }
   get "/topics/:id" => "topics#show", constraints: constraints, defaults: { format: :json }
   post "/reply/:post_id" => "posts#reply", constraints: constraints, defaults: { format: :json }
 end
