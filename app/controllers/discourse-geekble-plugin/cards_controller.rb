e0f7a69146a703307058010c5f9c67bd9623f17b
Yundo Han <han@yun.do>
2018-10-02 17:24:54 +0900

    add tags in cards_controller

--- app/controllers/discourse-geekble-plugin/cards_controller.rb
+++ app/controllers/discourse-geekble-plugin/cards_controller.rb
@@ -2,30 +2,32 @@ module DiscourseGeekblePlugin
   class CardsController < ApplicationController
     PAGE_SIZE = 20
     def top_index
-      card_category = Category.find_by(slug: 'cards')
       page = params[:page].to_i rescue 0
-      stmt = card_category.topics.where("deleted_at IS NULL")
-      stmt = stmt.where("last_posted_at IS NOT NULL")
-      stmt = stmt.order("score desc")
-      stmt = stmt.limit(PAGE_SIZE).offset(PAGE_SIZE * page)
+      stmt = card_topics(page).order("score desc")
       render json: topics_to_json(stmt)
     end
 
     def recent_index
-      card_category = Category.find_by(slug: 'cards')
       page = params[:page].to_i rescue 0
-      stmt = card_category.topics.where("deleted_at IS NULL")
-      stmt = stmt.where("last_posted_at IS NOT NULL")
-      stmt = stmt.order("last_posted_at desc")
-      stmt = stmt.limit(PAGE_SIZE).offset(PAGE_SIZE * page)
+      stmt = card_topics(page).order("last_posted_at desc")
       render json: topics_to_json(stmt)
     end
-    
+
     private
+    def card_topics(page)
+      card_category = Category.find_by(slug: 'cards').id
+      stmt = Topic.includes(:tags)
+      stmt = stmt.where(category_id: card_category_id)
+      stmt = stmt.where("deleted_at IS NULL")
+      stmt = stmt.where("last_posted_at IS NOT NULL")
+      stmt = stmt.limit(PAGE_SIZE).offset(PAGE_SIZE * page)
+    end
+
     def topics_to_json(stmt)
       @cards = []
       stmt.each do |c|
         card = c.as_json
+        card[:tags] = card.tags.map(&:name).as_json
         remarkable_posts = c.posts.where("like_count > 0").order('like_count desc').limit(1)
         card[:remarkable_post] = remarkable_posts.first.as_json if remarkable_posts.size
         card[:first_post] = c.posts.first.as_json
