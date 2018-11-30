cd457b6579d2abd5a6b67eee31910743b57bef06
Yundo Han <han@yun.do>
2018-11-30 16:56:09 +0900

    Optimize query

--- app/controllers/discourse-geekble-plugin/cards_controller.rb
+++ app/controllers/discourse-geekble-plugin/cards_controller.rb
@@ -1,6 +1,6 @@
 module DiscourseGeekblePlugin
   class CardsController < ApplicationController
-    PAGE_SIZE = 20
+    PAGE_SIZE = 15
     def top_index
       page = params[:page].to_i rescue 0
       stmt = card_topics(page).order("score desc")
@@ -16,7 +16,7 @@ module DiscourseGeekblePlugin
     private
     def card_topics(page)
       card_category_id = Category.find_by(slug: 'cards').id
-      stmt = Topic.includes(:tags)
+      stmt = Topic.includes(:tags).includes(posts: :user)
       stmt = stmt.where(category_id: card_category_id)
       stmt = stmt.where("deleted_at IS NULL")
       stmt = stmt.where("last_posted_at IS NOT NULL")
@@ -29,11 +29,23 @@ module DiscourseGeekblePlugin
         card = c.as_json
         card[:tags] = c.tags.map(&:name).as_json
         remarkable_posts = c.posts.where("like_count > 0").order('like_count desc').limit(1)
-        card[:remarkable_post] = remarkable_posts.first.as_json if remarkable_posts.size
-        card[:first_post] = c.posts.first.as_json
+        if remarkable_posts.size
+          card[:remarkable_post] = remarkable_posts.first.as_json.merge({
+            user: extract_user(remarkable_posts.first.user)
+          })
+        end
+        card[:first_post] = c.posts.order(created_at: :asc).first.as_json
         @cards.push card
       end
       @cards
+    end
+
+    private
+    def extract_user(user)
+      { 
+        id: user.id, username: user.username, name: user.name, admin: user.admin,
+        avatar_template: user.avatar_template,
+      }
     end
   end
 
